495e404dd81fa5d3ec3fe723b08db5bfedd5f403
JeZaKa <antycode17@gmail.com>
2026-04-22 13:12:09 +0300

    feat: add custom fonts and improve layout loading with splash screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 import {styled} from "nativewind";
 
@@ -8,8 +8,8 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-sans-extrabold text-primary">
+        Home
       </Text>
       <Link href="/(auth)/sign-in" className="mt-4 text-primary bg-primary/10 px-4 py-2 rounded">
         Sign In
