39d198c578dc51b750388567966fc40986a7a8d6
JeZaKa <antycode17@gmail.com>
2026-04-22 12:56:20 +0300

    feat: implement tab layout and add icons, themes, and assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import {styled} from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 p-5 bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -13,6 +17,6 @@ export default function App() {
       <Link href="/(auth)/sign-up" className="mt-2 text-primary bg-primary/10 px-4 py-2 rounded">
         Sign Up
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
